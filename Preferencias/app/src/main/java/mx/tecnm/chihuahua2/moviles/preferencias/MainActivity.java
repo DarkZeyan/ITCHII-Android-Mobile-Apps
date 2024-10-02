7750615bfea5be3ec39a7f56eecd21ff5f4e36da
Jorge Bugarini <l21550317@chihuahua2.tecnm.mx>
2024-10-02 15:51:41 -0600

    Progreso en practica de preferencias

---
+++ Preferencias/app/src/main/java/mx/tecnm/chihuahua2/moviles/preferencias/MainActivity.java
@@ -0,0 +1,37 @@
+package mx.tecnm.chihuahua2.moviles.preferencias;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText_name;
+    RadioButton radioButton_plant;
+    RadioButton radioButton_animal;
+    RadioButton radioButton_figure;
+    TextView textView_birth_date;
+    CalendarView calendarView_birth_date;
+    Button button_save;
+    Button button_view;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
