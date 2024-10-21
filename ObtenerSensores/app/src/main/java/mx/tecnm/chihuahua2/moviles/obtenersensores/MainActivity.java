0390c6b5d4ea8be16cfaf3c3aa2d9e437a37e5bd
Jorge Bugarini <l21550317@chihuahua2.tecnm.mx>
2024-10-21 14:27:19 -0600

    Sincronizacion del proyecto entre dispositivos

---
+++ ObtenerSensores/app/src/main/java/mx/tecnm/chihuahua2/moviles/obtenersensores/MainActivity.java
@@ -0,0 +1,57 @@
+package mx.tecnm.chihuahua2.moviles.obtenersensores;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Objeto para la gestion de sensores
+    private SensorManager sensorManager;
+    // Lista para el guardado del listado
+    ListView listView_sensores;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        listView_sensores = findViewById(R.id.listView_sensores);
+//        Para la identificacion de los sensores que estan disponibles en el dispositivo,
+//        se debe obtener una referencia al servicio del sensor
+
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        // Despues de la instanciacion del sensorManager se obtiene la lista de sensores disponibles
+        List<Sensor> listaSensores = sensorManager.getSensorList(Sensor.TYPE_ALL);
+
+//        Se crea un adaptador, el cual es un mecanismo de Android que funciona como puente entre nuestros datos
+        // y las vistas contenidas en el ListView (lista_sensores), puede ser tambien un GridView o un Spinner
+
+        final ArrayAdapter<Sensor> adaptador = new ArrayAdapter<>(MainActivity.this,android.R.layout.simple_list_item_1,listaSensores);
+
+        // Se le asignan los datos de la listview al adaptador
+        listView_sensores.setAdapter(adaptador);
+
+
+
+    }
+}
